695310d2956de4f3fa9c54abbe7dbd2774b138aa
Cory O'Daniel <cory@coryodaniel.com>
2022-01-17 14:41:02 -0800

    example

--- examples/main.tf
+++ examples/main.tf
@@ -9,8 +9,25 @@ terraform {
 
 provider "massdriver" {}
 
-resource "massdriver_artifact" {
-  artifact = jsonencode({"hello"="world"})
+resource "massdriver_artifact" "vpc" {
+  artifact = jsonencode(
+    {
+      field_name = "vpc"
+      provider_resource_id = aws_vpc.main.arn
+      type                 = "aws-ec2-vpc"
+      name                 = "VPC ${var.md_name_prefix} (${aws_vpc.main.id})"
+      data = { 
+        arn = aws_iam_role.foo.arn
+      }
+      specs = {
+        aws = {
+          region = "us-west-2"
+          service = "iam"
+          resource = "role"
+        }
+      }      
+    }
+  )
 }
 
 output "artifact" {
