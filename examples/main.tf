9f1a46beb9835d68b39e1d74458e8bbfd411f531
Cory O'Daniel <cory@coryodaniel.com>
2022-01-17 17:19:43 -0800

    Update main.tf

--- examples/main.tf
+++ examples/main.tf
@@ -12,18 +12,22 @@ provider "massdriver" {}
 resource "massdriver_artifact" "vpc" {
   artifact = jsonencode(
     {
-      # The field in the bundle's output artifacts schema.
-      field = "vpc"
-      # The unique ID from the cloud provider
-      provider_resource_id = aws_vpc.main.arn
-      # The artifact definition type
-      type                 = "aws-ec2-vpc"
-      # A friendly name, overridable by user
-      name                 = "VPC ${var.md_name_prefix} (${aws_vpc.main.id})"
-      # secret data
+      metadata = {
+        # The field in the bundle's output artifacts schema.
+        field = "vpc"
+        # The unique ID from the cloud provider
+        provider_resource_id = aws_vpc.main.arn
+        # The artifact definition type
+        type                 = "aws-ec2-vpc"
+        # A friendly name, overridable by user
+        name                 = "VPC ${var.md_name_prefix} (${aws_vpc.main.id})"
+        # secret data
+      }
+      
       data = { 
         arn = aws_iam_role.foo.arn
       }
+      
       # search / filtering / matching specs
       specs = {
         aws = {
