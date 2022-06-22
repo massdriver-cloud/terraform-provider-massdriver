90300ecac7c793508436f52952eb3277a1bbd0ab
chrisghill <chris@massdriver.cloud>
2022-06-22 02:09:53 -0600

    Updating and generating documentation for the terraform registry

--- examples/main.tf
+++ examples/main.tf
@@ -12,16 +12,16 @@ provider "massdriver" {}
 resource "massdriver_artifact" "vpc" {
 
   # The field in the bundle's output artifacts schema.
-  field                = "vpc"
+  field = "vpc"
   # The unique ID from the cloud provider
   provider_resource_id = aws_vpc.main.arn
   # The artifact definition type
-  type                 = "aws-ec2-vpc"
+  type = "aws-ec2-vpc"
   # A friendly name, overridable by user
-  name                 = "VPC ${var.md_name_prefix} (${aws_vpc.main.id})"
+  name = "VPC ${var.md_name_prefix} (${aws_vpc.main.id})"
 
   artifact = jsonencode(
-    {      
+    {
       data = {
         infrastructure = {
           arn = aws_iam_role.foo.arn
@@ -30,11 +30,11 @@ resource "massdriver_artifact" "vpc" {
       # search / filtering / matching specs
       specs = {
         aws = {
-          region = "us-west-2"
-          service = "iam"
+          region   = "us-west-2"
+          service  = "iam"
           resource = "role"
         }
-      }      
+      }
     }
   )
 }
@@ -44,9 +44,9 @@ resource "massdriver_package_alarm" "high_cpu" {
   // AWS
   cloud_resource_id = aws_cloudwatch_metric_alarm.alarm.arn
   // GCP
-  cloud_resource_id = google_monitoring_alert_policy.alert_policy.name
+  // cloud_resource_id = google_monitoring_alert_policy.alert_policy.name
   // Azure
-  cloud_resource_id = azurerm_monitor_metric_alert.main.id
+  // cloud_resource_id = azurerm_monitor_metric_alert.main.id
 }
 
 output "artifact" {
--- massdriver/resource_package_alarm.go
+++ massdriver/resource_package_alarm.go
@@ -15,6 +15,8 @@ type PackageAlarmMetadata struct {
 
 func resourcePackageAlarm() *schema.Resource {
 	return &schema.Resource{
+		Description: "This resource registers a package alarm in the Massdriver console for presentation to the user",
+
 		CreateContext: resourcePackageAlarmCreate,
 		ReadContext:   schema.NoopContext,
 		UpdateContext: resourcePackageAlarmUpdate,
@@ -22,19 +24,21 @@ func resourcePackageAlarm() *schema.Resource {
 
 		Schema: map[string]*schema.Schema{
 			"cloud_resource_id": {
+				Description: "The identifier of the alarm. In Azure it will be the id, GCP will be the name, and in AWS it will be the arn",
 				Type:        schema.TypeString,
 				Required:    true,
-				Description: "The identifier of the alarm. In Azure it will be the id, GCP will be the name, and in AWS it will be the arn",
 			},
 			"display_name": {
+				Description: "The name to display in the massdriver UI",
 				Type:        schema.TypeString,
 				Required:    true,
-				Description: "The name to display in the massdriver UI",
 			},
 			"last_updated": {
-				Type:     schema.TypeString,
-				Optional: true,
-				Computed: true,
+				Description: "A timestamp of when the last time this resource was updated",
+				Type:        schema.TypeString,
+				Optional:    false,
+				Required:    false,
+				Computed:    true,
 			},
 		},
 	}
